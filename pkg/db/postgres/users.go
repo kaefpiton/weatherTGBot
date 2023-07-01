71273cf8cfa7b8f0b22182377b3670b940fcaf51
dfedchenkov <dfedchenkov@team.amocrm.com>
2023-07-01 17:42:25 +0300

    update user db model📌

--- pkg/db/postgres/users.go
+++ pkg/db/postgres/users.go
@@ -6,15 +6,15 @@ import (
 	"time"
 )
 
-type Users struct {
+type User struct {
 	ID        int64
 	Firstname string
 	Lastname  string
 	ChatID    int64
 }
 
-func (db *DB) InsertUser(usersFirstname, usersLastname string, chatid int64) error {
-	userID, err := getUserIDByChatID(db, chatid)
+func (db *DB) InsertUser(firstname, lastname string, chatID int64) error {
+	userID, err := getUserIDByChatID(db, chatID)
 
 	if err != nil {
 		return err
@@ -23,43 +23,46 @@ func (db *DB) InsertUser(usersFirstname, usersLastname string, chatid int64) err
 	if userID == -1 {
 		//todo прикрутить лог
 		fmt.Println("Создание нового пользователя")
-		return createUser(db, usersFirstname, usersLastname, chatid)
+		return createUser(db, firstname, lastname, chatID)
 	} else {
 		fmt.Println("Пользователь с chatID существует - меняем дату")
 		return updateLastUsage(db, userID)
 	}
 }
 
-func createUser(db *DB, usersFirstname, usersLastname string, chatID int64) error {
-	var user Users
-
-	user.Firstname = usersFirstname
-	user.Lastname = usersLastname
-	user.ChatID = chatID
+func createUser(db *DB, firstname, lastname string, chatID int64) error {
+	user := User{
+		Firstname: firstname,
+		Lastname:  lastname,
+		ChatID:    chatID,
+	}
 
 	_, err := db.Exec("INSERT INTO users (firstname, lastname, chat_id) values ($1, $2, $3)",
 		user.Firstname,
 		user.Lastname,
 		user.ChatID)
+
 	return err
 }
 
 func updateLastUsage(db *DB, userID int64) error {
-	_, err := db.Exec("UPDATE users SET last_usage = $1 WHERE chat_id = $2",
+	_, err := db.Exec("UPDATE users SET last_usage = $1 WHERE id = $2",
 		time.Now(),
 		userID)
 
 	return err
 }
+
+// todo возможно primary сделать chatID (посмотреть не повторяются ли)
 func getUserIDByChatID(db *DB, chatID int64) (int64, error) {
 	stmt, err := db.Prepare("SELECT ID FROM users WHERE chat_id = $1")
 	if err != nil {
 		return 0, err
 	}
 
-	var user Users
+	var userID int64
 
-	err = stmt.QueryRow(chatID).Scan(&user.ID)
+	err = stmt.QueryRow(chatID).Scan(&userID)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return -1, nil
@@ -67,5 +70,5 @@ func getUserIDByChatID(db *DB, chatID int64) (int64, error) {
 
 		return -1, err
 	}
-	return chatID, nil
+	return userID, nil
 }
